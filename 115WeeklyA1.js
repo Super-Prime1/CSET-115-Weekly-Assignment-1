a61461456da5c75b099ef2dc31c91f1998a3e924
Super-Prime1 <superprime1@icloud.com>
2025-10-06 08:03:12 -0400

    mostly finished project added history, clear, delete

--- 115WeeklyA1.js
+++ 115WeeklyA1.js
@@ -2,37 +2,39 @@ var func = "";
 var answer = 0;
 var num1 = 0;
 var num2 = 0;
+let history = [];
 
-// document.getElementById("zero").onclick = function click(){
-//     updateCalc(0);
-// }
+
+document.getElementById("zero").onclick = function click(){
+    updateCalc(0);
+}
 document.getElementById("one").onclick = function click(){
     updateCalc(1);
 }
 document.getElementById("two").onclick = function click(){
     updateCalc(2);
 }
-// document.getElementById("three").onclick = function click(){
-//     updateCalc(3);
-// };
-// document.getElementById("four").onclick = function click(){
-//     updateCalc(4);
-// };
-// document.getElementById("five").onclick = function click(){
-//     updateCalc(5);
-// };
-// document.getElementById("six").onclick = function click(){
-//     updateCalc(6);
-// };
-// document.getElementById("seven").onclick = function click(){
-//     updateCalc(7);
-// };
-// document.getElementById("eight").onclick = function click(){
-//     updateCalc(8);
-// };
-// document.getElementById("nine").onclick = function click(){
-//     updateCalc(9);
-// };
+document.getElementById("three").onclick = function click(){
+    updateCalc(3);
+};
+document.getElementById("four").onclick = function click(){
+    updateCalc(4);
+};
+document.getElementById("five").onclick = function click(){
+    updateCalc(5);
+};
+document.getElementById("six").onclick = function click(){
+    updateCalc(6);
+};
+document.getElementById("seven").onclick = function click(){
+    updateCalc(7);
+};
+document.getElementById("eight").onclick = function click(){
+    updateCalc(8);
+};
+document.getElementById("nine").onclick = function click(){
+    updateCalc(9);
+};
 
 
 
@@ -44,56 +46,82 @@ function updateCalc(value){
 
 
 function add(){
-    num1 = parseInt(document.getElementById("calc").innerText || 0);
+    num1 = parseFloat(document.getElementById("calc").innerText || 0);
     document.getElementById("calc").innerText = "";
     func = "add";
     console.log(func);
 }
 
 function sub(){
-    num1 = parseInt(document.getElementById("calc").innerText || 0);
+    num1 = parseFloat(document.getElementById("calc").innerText || 0);
     document.getElementById("calc").innerText = "";
     func = "sub";
     console.log(func);
 }
 
 function mul(){
-    num1 = parseInt(document.getElementById("calc").innerText || 0);
+    num1 = parseFloat(document.getElementById("calc").innerText || 0);
     document.getElementById("calc").innerText = "";
     func = "mul";
     console.log(func);
 }
 function div(){
-    num1 = parseInt(document.getElementById("calc").innerText || 0);
+    num1 = parseFloat(document.getElementById("calc").innerText || 0);
     document.getElementById("calc").innerText = "";
     func = "div";
     console.log(func);
 }
 
+function cle(){
+    calc.innerText = "";
+}
+
+function dot(){
+    calc.innerText += "."
+}
+
+function del(){
+    calc.innerText = calc.innerText.slice(0,-1);
+}
+
+function his(){
+    console.log(`History:`)
+    for(let i =0;i<history.length;i++){
+        console.log(history[i]);
+    }
+}
+
+
 function finish(){
-    num2 = parseInt(document.getElementById("calc").innerText);
-    console.log(num2);
+    num2 = parseFloat(document.getElementById("calc").innerText);
     if(func === "add"){
         answer = num1 + num2;
         calc.innerText = answer;
+        history.push(`${num1} + ${num2} = ${num1 + num2}`);
     }
     if(func === "sub"){
         answer = num1 - num2;
         calc.innerText = answer;
+        history.push(`${num1} - ${num2} = ${num1 - num2}`);
+
     }
-        if(func === "mul"){
+    if(func === "mul"){
         answer = num1 * num2;
         calc.innerText = answer;
+        history.push(`${num1} * ${num2} = ${num1 * num2}`);
+
     }
-            if(func === "div"){
+    if(func === "div"){
         answer = num1 / num2;
         calc.innerText = answer;
+        history.push(`${num1} / ${num2} = ${num1 / num2}`);
+
     }
 }
 
 
 function change(){
-    let num = parseInt(document.getElementById("calc").innerText || 0);
+    let num = parseFloat(document.getElementById("calc").innerText || 0);
     num = num * -1;
     document.getElementById("calc").innerText = num;
 
